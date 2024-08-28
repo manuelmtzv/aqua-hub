80bb603b08f0c4d8b4b4bd40df459018fe053f15
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-08-27 22:57:50 -0600

    feat: add TheLogo component to MiniHeader

--- components/the/MiniHeader.vue
+++ components/the/MiniHeader.vue
@@ -4,7 +4,9 @@ const { user } = useAuthState();
 </script>
 
 <template>
-  <div class="py-6 flex justify-between items-center">
+  <div class="py-6 flex justify-between items-center min-h-20">
+    <TheLogo v-if="!isSidebarOpen" />
+
     <p v-if="user" class="ml-auto">{{ `${user.name} ${user.lastname}` }}</p>
   </div>
 </template>

1858cd602a2c099ad6440744d6df3130488210e2
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-09-15 13:22:31 -0600

    refactor: remove button custom class and add classes to Form/Button component

--- components/form/Button.vue
+++ components/form/Button.vue
@@ -9,7 +9,12 @@ defineProps<FormButtonProps>();
 
 <template>
   <button
-    :class="cn('button', $props.class)"
+    :class="
+      cn(
+        'inline-block px-4 py-2 rounded-md shadow-sm transition-colors duration-200',
+        $props.class
+      )
+    "
     type="submit"
     :disabled="disabled"
   >

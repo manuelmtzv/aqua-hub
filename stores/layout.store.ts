d14a7dc16369ad9c3d4838cc173c90d250838605
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-09-16 17:48:01 -0600

    feat: hide sidebar if screen is small

--- stores/layout.store.ts
+++ stores/layout.store.ts
@@ -1,13 +1,25 @@
+import { useWindowSize } from "@vueuse/core";
 import { defineStore } from "pinia";
 
 export const useLayoutStore = defineStore(
   "layoutStore",
   () => {
     const isSidebarOpen = ref(true);
+    const { width } = useWindowSize();
+
+    watchEffect(() => {
+      if (width.value < 1024 && isSidebarOpen.value) {
+        isSidebarOpen.value = false;
+      }
+    });
 
     return {
       isSidebarOpen,
       toggleSidebar: () => {
+        if (width.value < 1024) {
+          isSidebarOpen.value = false;
+        }
+
         isSidebarOpen.value = !isSidebarOpen.value;
       },
     };

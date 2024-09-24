55c76989d3fbabcc6124851e200a3aa88bf175c7
Your Name <manuel.mtzv816@gmail.com>
2024-09-24 14:47:41 -0600

    fix: correctly send headers in the custom $fetch instance

--- plugins/api.ts
+++ plugins/api.ts
@@ -6,12 +6,16 @@ export default defineNuxtPlugin({
 
     const api = $fetch.create({
       baseURL: API_BASE_URL,
-      onRequest(context) {
-        context.options.headers = {
-          ...context.options.headers,
-          "Accept-Language": $i18n.locale.value,
-          Authorization: `Bearer ${useCookie("access_token").value}`,
-        };
+      onRequest({ options }) {
+        const headers = (options.headers ||= {} as Headers);
+
+        // Check the reactive property access
+        addHeader(headers, "Accept-Language", $i18n.locale);
+        addHeader(
+          headers,
+          "Authorization",
+          `Bearer ${useCookie("access_token").value}`
+        );
       },
     });
 
@@ -22,3 +26,18 @@ export default defineNuxtPlugin({
     };
   },
 });
+
+function addHeader(
+  headers: Headers | Array<Record<string, any>> | unknown,
+  key: string,
+  value: string
+) {
+  if (Array.isArray(headers)) {
+    headers.push([key, value]);
+  } else if (headers instanceof Headers) {
+    headers.set(key, value);
+  } else {
+    // Ensure the original object is mutated
+    (headers as Record<string, any>)[key] = value;
+  }
+}

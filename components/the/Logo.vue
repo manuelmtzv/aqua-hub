1782c495bfe2f148541220163b24b281f3b8f513
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-09-28 16:01:12 -0600

    feat: validate the logo route by the user presence

--- components/the/Logo.vue
+++ components/the/Logo.vue
@@ -5,10 +5,17 @@ type LogoProps = {
 };
 
 defineProps<LogoProps>();
+const localePath = useLocalePath();
+
+const route = computed(() => {
+  const { user } = useAuthState();
+
+  return user ? localePath("/app") : localePath("/");
+});
 </script>
 
 <template>
-  <NuxtLink to="/" :class="[cn('text-xl font-medium', leftClass)]"
+  <NuxtLink :to="route" :class="[cn('text-xl font-medium', leftClass)]"
     >Fishkeepers<span :class="[cn('text-xl underline font-medium', rightClass)]"
       >Hub</span
     ></NuxtLink

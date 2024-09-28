d88a4be4a8cb98198534e5e9f0525e4641f48c10
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-09-28 10:21:37 -0600

    style: reduce width of expanded sidebar

--- components/the/Sidebar.vue
+++ components/the/Sidebar.vue
@@ -6,7 +6,7 @@ const { isSidebarOpen } = useLayoutState();
   <aside
     :class="[
       'sidebar min-h-[100svh] max-h-[100svh] px-5 py-6 border-r flex flex-col gap-5 overflow-hidden top-0 z-10 sticky dark:border-dark-500',
-      isSidebarOpen ? 'min-w-[300px]' : 'min-w-[0px]',
+      isSidebarOpen ? 'min-w-[17rem]' : 'min-w-[0px]',
     ]"
     ref="sidebarRef"
   >
